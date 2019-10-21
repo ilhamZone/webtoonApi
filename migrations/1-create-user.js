7072c34014f57db2b51ecf006489c3c4d6d36467
unknown <iam.mhd07@gmail.com>
2019-10-21 23:09:39 +0700

    addddd

--- migrations/1-create-user.js
+++ migrations/1-create-user.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
